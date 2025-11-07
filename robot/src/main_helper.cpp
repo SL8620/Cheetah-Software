aa0175c68f42dddaa4b845a5fdd8ed79542c29ff
SL8620 <2584032673@qq.com>
2025-11-07 18:31:52 +0800

    ab943ca完成,正常编译运行

--- robot/src/main_helper.cpp
+++ robot/src/main_helper.cpp
@@ -1,7 +1,8 @@
 /*!
  * @file main.cpp
  * @brief Main Function for the robot program
- *
+ * Robot启动文件 
+ * 不用改别的了
  * The main function parses command line arguments and starts the appropriate
  * driver.
  */
@@ -19,90 +20,128 @@ MasterConfig gMasterConfig;
 /*!
  * Print a message describing the command line flags for the robot program
  */
-void printUsage() {
-  printf(
-      "Usage: robot [robot-id] [sim-or-robot] [parameters-from-file]\n"
-      "\twhere robot-id:     3 for cheetah 3, m for mini-cheetah\n"
-      "\t      sim-or-robot: s for sim, r for robot\n"
-      "\t      param-file:   f for loading parameters from file, l (or nothing) for LCM\n"
-      "                      this option can only be used in robot mode\n");
+void printUsage() 
+{
+	printf(
+		"Usage: robot [robot-id] [sim-or-robot] [parameters-from-file]\n"
+		"\twhere robot-id:     3 for cheetah 3, m for mini-cheetah\n"
+		"\t      sim-or-robot: s for sim, r for robot\n"
+		"\t      param-file:   f for loading parameters from file, l (or nothing) for LCM\n"
+		"                      this option can only be used in robot mode\n");
 }
 
 /*!
  * Setup and run the given robot controller
  */
-int main_helper(int argc, char** argv, RobotController* ctrl) {
-  if (argc != 3 && argc != 4) {
-    printUsage();
-    return EXIT_FAILURE;
-  }
+int main_helper(int argc, char** argv, RobotController* ctrl) 
+{
+	// 传入参数数目不是3或4,则输出提示错误
+	if (argc != 3 && argc != 4) 
+	{
+		printUsage();
+		return EXIT_FAILURE;
+	}
 
-  if (argv[1][0] == '3') {
-    gMasterConfig._robot = RobotType::CHEETAH_3;
-  } else if (argv[1][0] == 'm') {
-    gMasterConfig._robot = RobotType::MINI_CHEETAH;
-  } else {
-    printUsage();
-    return EXIT_FAILURE;
-  }
+	// 3表示cheetah 3，m表示mini-cheetah
+	if (argv[1][0] == '3') 
+	{
+		gMasterConfig._robot = RobotType::CHEETAH_3;
+	} 
+	else if (argv[1][0] == 'm') 
+	{
+		gMasterConfig._robot = RobotType::MINI_CHEETAH;
+	} 
+	else 
+	{
+		printUsage();
+		return EXIT_FAILURE;
+	}
 
-  if (argv[2][0] == 's') {
-    gMasterConfig.simulated = true;
-  } else if (argv[2][0] == 'r') {
-    gMasterConfig.simulated = false;
-  } else {
-    printUsage();
-    return EXIT_FAILURE;
-  }
+	// s表示仿真，r表示实物
+	if (argv[2][0] == 's') 
+	{
+		gMasterConfig.simulated = true;
+	} 
+	else if (argv[2][0] == 'r') 
+	{
+		gMasterConfig.simulated = false;
+	} 
+	else 
+	{
+		printUsage();
+		return EXIT_FAILURE;
+	}
 
-  if(argc == 4 && argv[3][0] == 'f') {
-    gMasterConfig.load_from_file = true;
-    printf("Load parameters from file\n");
-  } else {
-    gMasterConfig.load_from_file = false;
-    printf("Load parameters from network\n");
-  }
+	// 如果有第四个参数，l表示LCM，f表示file
+	if(argc == 4 && argv[3][0] == 'f') 
+	{
+		gMasterConfig.load_from_file = true;
+		printf("Load parameters from file\n");
+	} 
+	else 
+	{
+		gMasterConfig.load_from_file = false;
+		printf("Load parameters from network\n");
+	}
 
-  printf("[Quadruped] Cheetah Software\n");
-  printf("        Quadruped:  %s\n",
-         gMasterConfig._robot == RobotType::MINI_CHEETAH ? "Mini Cheetah"
-                                                         : "Cheetah 3");
-  printf("        Driver: %s\n", gMasterConfig.simulated
-                                     ? "Development Simulation Driver"
-                                     : "Quadruped Driver");
+	// 打印运行信息
+	printf("[Quadruped] Cheetah Software\n");
+	printf("        Quadruped:  %s\n",
+			gMasterConfig._robot == RobotType::MINI_CHEETAH ? "Mini Cheetah"
+															: "Cheetah 3");
+	printf("        Driver: %s\n", gMasterConfig.simulated
+										? "Development Simulation Driver"
+										: "Quadruped Driver");
 
-  // dispatch the appropriate driver
-  if (gMasterConfig.simulated) {
-    if(argc != 3) {
-      printUsage();
-      return EXIT_FAILURE;
-    }
-    if (gMasterConfig._robot == RobotType::MINI_CHEETAH) {
-      SimulationBridge simulationBridge(gMasterConfig._robot, ctrl);
-      simulationBridge.run();
-      printf("[Quadruped] SimDriver run() has finished!\n");
-    } else if (gMasterConfig._robot == RobotType::CHEETAH_3) {
-      SimulationBridge simulationBridge(gMasterConfig._robot, ctrl);
-      simulationBridge.run();
-    } else {
-      printf("[ERROR] unknown robot\n");
-      assert(false);
-    }
-  } else {
+	// 仿真会仿真的程序
+	if (gMasterConfig.simulated) 
+	{
+		if(argc != 3) 
+		{
+			printUsage();
+			return EXIT_FAILURE;
+		}
+
+		// 模拟器创建SimulationBridge对象
+		if (gMasterConfig._robot == RobotType::MINI_CHEETAH) 
+		{
+			SimulationBridge simulationBridge(gMasterConfig._robot, ctrl);
+			simulationBridge.run();
+			printf("[Quadruped] SimDriver run() has finished!\n");
+		} 
+		else if (gMasterConfig._robot == RobotType::CHEETAH_3) 
+		{
+			SimulationBridge simulationBridge(gMasterConfig._robot, ctrl);
+			simulationBridge.run();
+		} 
+		else 
+		{
+			printf("[ERROR] unknown robot\n");
+			assert(false);
+		}
+	} 
+	else 
+	{
 #ifdef linux
-    if (gMasterConfig._robot == RobotType::MINI_CHEETAH) {
-      MiniCheetahHardwareBridge hw(ctrl, gMasterConfig.load_from_file);
-      hw.run();
-      printf("[Quadruped] SimDriver run() has finished!\n");
-    } else if (gMasterConfig._robot == RobotType::CHEETAH_3) {
-      Cheetah3HardwareBridge hw(ctrl);
-      hw.run();
-    } else {
-      printf("[ERROR] unknown robot\n");
-      assert(false);
-    }
-#endif
-  }
+		// 实物创建四足机器人对象对象
+		if (gMasterConfig._robot == RobotType::MINI_CHEETAH) 
+		{
+			MiniCheetahHardwareBridge hw(ctrl, gMasterConfig.load_from_file);
+			hw.run();
+			printf("[Quadruped] SimDriver run() has finished!\n");
+		} 
+		else if (gMasterConfig._robot == RobotType::CHEETAH_3) 
+		{
+			Cheetah3HardwareBridge hw(ctrl);
+			hw.run();
+		} 
+		else 
+		{
+			printf("[ERROR] unknown robot\n");
+			assert(false);
+		}
+	#endif
+	}
 
-  return 0;
+	return 0;
 }
--- user/MIT_Controller/MIT_Controller.cpp
+++ user/MIT_Controller/MIT_Controller.cpp
@@ -6,33 +6,31 @@ MIT_Controller::MIT_Controller():RobotController(){  }
 /**
  * Initializes the Control FSM.
  */
-void MIT_Controller::initializeController() {
-  // Initialize a new GaitScheduler object
-  _gaitScheduler = new GaitScheduler<float>(&userParameters, _controlParameters->controller_dt);
+void MIT_Controller::initializeController() 
+{
+    // 初始化一个新的GaitScheduler对象
+    _gaitScheduler = new GaitScheduler<float>(&userParameters, _controlParameters->controller_dt);
 
-  // Initialize a new ContactEstimator object
-  //_contactEstimator = new ContactEstimator<double>();
-  ////_contactEstimator->initialize();
-
-  // Initializes the Control FSM with all the required data
-  _controlFSM = new ControlFSM<float>(_quadruped, _stateEstimator,
-                                      _legController, _gaitScheduler,
-                                      _desiredStateCommand, _controlParameters, 
-                                      _visualizationData, &userParameters);
+    // 所有需要的数据初始化控制FSM
+    _controlFSM = new ControlFSM<float>(_quadruped, _stateEstimator,
+                                        _legController, _gaitScheduler,
+                                        _desiredStateCommand, _controlParameters, 
+                                        _visualizationData, &userParameters);
 }
 
 /**
  * Calculate the commands for the leg controllers using the ControlFSM logic.
  */
-void MIT_Controller::runController() {
-  // Find the current gait schedule
-  _gaitScheduler->step();
+void MIT_Controller::runController() 
+{
+    // 查找当前步态计划
+    _gaitScheduler->step();
 
-  // Find the desired state trajectory
-  _desiredStateCommand->convertToStateCommands();
+    // 查找期望状态轨迹
+    _desiredStateCommand->convertToStateCommands();
 
-  // Run the Control FSM code
-  _controlFSM->runFSM();
+    // 运行控制FSM代码
+    _controlFSM->runFSM();
 }
 
 
